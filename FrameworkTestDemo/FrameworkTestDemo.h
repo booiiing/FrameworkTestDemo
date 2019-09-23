36d4ee4b6058f6932b67f2ae1830f44c2d4e941e
Patric Schenke <patric.schenke@de.bosch.com>
2019-09-23 14:48:26 +0200

    Initial Commit

---
+++ FrameworkTestDemo/FrameworkTestDemo.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkTestDemo.h
+//  FrameworkTestDemo
+//
+//  Created by Patric Schenke on 23.09.19.
+//  Copyright © 2019 Patric Schenke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkTestDemo.
+FOUNDATION_EXPORT double FrameworkTestDemoVersionNumber;
+
+//! Project version string for FrameworkTestDemo.
+FOUNDATION_EXPORT const unsigned char FrameworkTestDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkTestDemo/PublicHeader.h>
+
+
---
+++ FrameworkTestDemoTests/FrameworkTestDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  FrameworkTestDemoTests.m
+//  FrameworkTestDemoTests
+//
+//  Created by Patric Schenke on 23.09.19.
+//  Copyright © 2019 Patric Schenke. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FrameworkTestDemoTests : XCTestCase
+
+@end
+
+@implementation FrameworkTestDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
